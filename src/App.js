70b9082fcfa338f3260925f990cefd3a104a5a68
Joshua Burger <joshberger6969@outlook.com>
2022-07-08 13:47:44 -0400

    Import useState and useEffect, Create array to use state for currentColorArrangement. Inside createboard function set the current color arrangement after function exits loop. Create useEffect to call createBoard.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { useEffect, useState } from 'react'
 
 
 const width = 8;
@@ -13,17 +14,25 @@ const candyColors = [
 
 const App = () => {
 
+  const [currentColorArrangement, setCurrentColorArrangement] = useState([])
+
   const createBoard = () => {
     const randomColorArrangment = []
     for (let i = 0; i < width * width; i++) {
       const randomColor = candyColors[Math.floor(Math.random() * candyColors.length)]
       randomColorArrangment.push(randomColor)
     }
+    setCurrentColorArrangement(randomColorArrangment)
   }
+
+  useEffect(() => {
+    createBoard()
+  }, [])
 
 
   return (
     <div>
+      
     </div>
   );
 }
