4a0be15f3cb9c818c14ab6e1d5166d54bbbe457b
Joshua Burger <joshberger6969@outlook.com>
2022-07-11 14:18:03 -0400

    Create new react functional component for scoreboard that displays users score

---
+++ src/components/ScoreBoard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function ScoreBoard() {
+  return (
+    <>
+        <div className='score'>
+            <h1>ScoreBoard</h1>
+            <h3>HighScore: 60</h3>
+        </div>
+    </>
+  )
+}
